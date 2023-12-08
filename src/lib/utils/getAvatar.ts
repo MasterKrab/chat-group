f1094efa37b25e469c507db3cde8e08591731514
Enzo <47456460+MasterKrab@users.noreply.github.com>
2023-12-08 14:53:32 -0300

    fix: variable name

--- src/lib/utils/getAvatar.ts
+++ src/lib/utils/getAvatar.ts
@@ -1,4 +1,4 @@
 const getAvatar = (username: string, avatar?: string) =>
-	avatar || `https://api.dicebear.com/7.x/identicon/svg?seed=${name}`
+	avatar || `https://api.dicebear.com/7.x/identicon/svg?seed=${username}`
 
 export default getAvatar
